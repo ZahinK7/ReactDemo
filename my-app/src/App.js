f233598ebf05c251e2a757ec520a162c1e5c08b4
Zahin Khan <77297140+ZahinK7@users.noreply.github.com>
2021-02-01 22:11:22 -0500

    added buttons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 class App extends Component {
+
+  constructor(){
+  super();
+
+  this.state= {
+    string: 'Hello my boy!'
+  }
+}
+
+
   render() {
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code>aytz</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.string}</p>
+           <button onClick={() => this.setState({string: 'Hello my son'})}>
+           change salutation
+           </button> 
+        
+        
       </header>
     </div>
     );
